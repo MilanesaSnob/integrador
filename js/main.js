b4241287cce82d44e13b9f25417b030a2c776934
Martin <mmmele@gmail.com>
2019-11-01 19:25:23 -0300

    1- Subida a Github

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var miTitulo = document.querySelector("h1");
+miTitulo.innerHTML = "Hola Mundo";
+
+var miTexto = document.getElementById('txt');
+miTexto.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, similique. Corrupti deserunt voluptates impedit doloremque error iste sunt, reiciendis totam et omnis laudantium vel pariatur commodi cum recusandae. Et, magni!';
